d2d98903eaceed9209f3732964cc13c4664d522a
keithi16 <keithishimogava16@gmail.com>
2025-03-06 07:09:03 -0300

    first commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // return all clients in the database
+        return response()->json(Client::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // validate the request
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:clients',
+            'phone' => 'required'
+        ]);
+
+        // add a new client to the database
+        $client = Client::create($request->all());
+
+        return response()->json(
+            [
+                'message' => 'Client created successfulyy',
+                'data' => $client,
+            ], 200
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+ <?php
+
+use App\Http\Controllers\ClientController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/status', function() {
+    return response()->json( 
+        [
+            'status' => 'ok',
+            'message' => 'API is running'
+        ],
+        200
+    );
+});
+
+Route::apiResource('clients', ClientController::class);
